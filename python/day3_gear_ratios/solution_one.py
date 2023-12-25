17cc2e425e5d8b36b6caf6c2f5858e1495efa44a
Eric Crowley <eecmtf@gmail.com>
2023-12-25 14:06:13 -0700

    in progress solution, hard to think while watching football

--- python/day3_gear_ratios/solution_one.py
+++ python/day3_gear_ratios/solution_one.py
@@ -1,4 +1,5 @@
 from collections.abc import Generator
+from itertools import chain
 
 INPUT_FILE = "input.txt"
 
@@ -23,49 +24,34 @@ def identify_unique_symbols_in_data(records: list[str]) -> list[str]:
     return {char for char in records_as_str if not is_int(char)}
 
 
-def find_numbers_adjacent_to_symbols(
-    all_records: list[str],
-    target_symbols: set[str],
-    all_found_numbers: list[int] = None,
-):
-    if len(all_records) < 3:
-        return all_found_numbers
-    target_records = all_records[:3]
-    del all_records[:3]
+def find_symbols(line: list[str]) -> list[int]:
+    symbol_indices = []
+    for count, char in enumerate(line):
+        if char != "." and not is_int(char):
+            symbol_indices.append(count)
+    return symbol_indices
 
-    parsed_records_info = []
-    for rec_cnt, record in enumerate(target_records):
-        symbol_indices, numbers, possible_number = [], [], []
-        for char_cnt, char in enumerate(record):
-            if char == ".":
-                if possible_number:
-                    number_parts, location_indices = [], []
-                    for components in possible_number:
-                        number_parts.append(components[0])
-                        location_indices.append(components[1])
-                    numbers.append(
-                        {"num": int("".join(number_parts)), "ixs": location_indices}
-                    )
-                    possible_number.clear()
-            elif char in target_symbols:
-                symbol_indices.append(char_cnt)
-            elif is_int(char):
-                possible_number.append((char, char_cnt))
-        parsed_records_info.append(
-            {
-                "record_num": rec_cnt,
-                "found_numbers": numbers,
-                "found_symbol_indices": symbol_indices,
-            }
-        )
 
-    # TODO: Now that we have the three records with all numbers they contained identified,
-    # including valid indices that touch the numbers, need to write a function that takes
-    # these three records and returns the valid nums
-    x = 0
 
 
 if __name__ == "__main__":
     problem_data = [row[1] for row in read_input_data()]
     symbols = identify_unique_symbols_in_data(problem_data)
-    valid_numbers = find_numbers_adjacent_to_symbols(problem_data, symbols)
+
+    by_line_analysis = {}
+    for count, line in enumerate(problem_data):
+        by_line_analysis[count] = {
+            "line_number": count,
+            "symbol_indices": find_symbols(line),
+        }
+
+    for line_number in by_line_analysis.keys():
+        all_symbol_indices_nested = []
+        for target_line_number in range(line_number-1 if line_number != 0 else 0, line_number+2):
+            all_symbol_indices_nested.append(by_line_analysis[target_line_number]['symbol_indices'])
+        all_symbol_indices = list(chain.from_iterable(all_symbol_indices_nested))
+
+        x = 0
+    
+
+    x = 0
